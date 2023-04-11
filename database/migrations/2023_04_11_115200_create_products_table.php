9cc06ba43d95d9f9555b890e31c6d3d05387c58e
Maxime Mllt <mlltmax66@gmail.com>
2023-04-11 13:55:36 +0200

    create products table

---
+++ database/migrations/2023_04_11_115200_create_products_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('ref');
+            $table->integer('vat');
+            $table->integer('price_excluding_vat');
+            $table->integer('price');
+            $table->boolean('is_archived');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
