3a951bb143f5e5fa94aa20b483c8c75ca5ca7ac8
Maxime Mllt <mlltmax66@gmail.com>
2023-04-11 14:24:52 +0200

    create product factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Product>
+ */
+class ProductFactory extends Factory
+{
+    public function definition(): array
+    {
+        $vatArray = [10, 15, 20];
+        $vat = $vatArray[array_rand([10, 15, 20])];
+        $priceExcludingVat = rand(1000, 10000);
+        $price = $priceExcludingVat + ($priceExcludingVat * ($vat / 100));
+
+        return [
+            'name' => $this->faker->words(3, true),
+            'ref' => 'ref_'.$this->faker->uuid(),
+            'vat' => $vat,
+            'price_excluding_vat' => $priceExcludingVat,
+            'price' => $price,
+            'is_archived' => $this->faker->boolean(10),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -10,5 +11,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+        Product::factory(100)->create();
     }
 }
