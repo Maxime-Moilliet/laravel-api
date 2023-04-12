5c676cf44fa8697b241f76d6cd068106056c5be4
Maxime Mllt <mlltmax66@gmail.com>
2023-04-12 10:33:39 +0200

    test User can see list of products

---
+++ tests/Feature/Product/IndexProduct.php
@@ -0,0 +1,137 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\Product;
+
+use function Pest\Laravel\get;
+
+it('should be return list of products page 1', function () {
+    $products = Product::factory(20)->create();
+
+    $productList = [];
+    foreach ($products as $product) {
+        $productList[] = [
+            'id' => $product->id,
+            'name' => $product->name,
+            'ref' => $product->ref,
+            'vat' => $product->vat,
+            'price_excluding_vat' => $product->price_excluding_vat,
+            'price' => $product->price,
+            'is_archived' => $product->is_archived,
+            'created_at' => $product->created_at,
+            'updated_at' => $product->updated_at,
+        ];
+    }
+
+    userLogin()->get(uri: '/api/products')
+        ->assertStatus(status: 200)
+        ->assertExactJson([
+            'data' => array_splice($productList, 0, 10),
+            'links' => [
+                'first' => 'http://localhost/api/products?page=1',
+                'last' => 'http://localhost/api/products?page=2',
+                'prev' => null,
+                'next' => 'http://localhost/api/products?page=2'
+            ],
+            'meta' => [
+                'current_page' => 1,
+                'from' => 1,
+                'last_page' => 2,
+                'links' => [
+                    [
+                        'url' => null,
+                        'label' => '&laquo; Previous',
+                        'active' => false
+                    ],
+                    [
+                        'url' => 'http://localhost/api/products?page=1',
+                        'label' => '1',
+                        'active' => true
+                    ],
+                    [
+                        'url' => 'http://localhost/api/products?page=2',
+                        'label' => '2',
+                        'active' => false
+                    ],
+                    [
+                        'url' => 'http://localhost/api/products?page=2',
+                        'label' => 'Next &raquo;',
+                        'active' => false
+                    ]
+                ],
+                'path' => 'http://localhost/api/products',
+                'per_page' => 10,
+                'to' => 10,
+                'total' => 20
+            ],
+        ]);
+});
+
+it('should be return list of products page 2', function () {
+    $products = Product::factory(20)->create();
+
+    $productList = [];
+    foreach ($products as $product) {
+        $productList[] = [
+            'id' => $product->id,
+            'name' => $product->name,
+            'ref' => $product->ref,
+            'vat' => $product->vat,
+            'price_excluding_vat' => $product->price_excluding_vat,
+            'price' => $product->price,
+            'is_archived' => $product->is_archived,
+            'created_at' => $product->created_at,
+            'updated_at' => $product->updated_at,
+        ];
+    }
+
+    userLogin()->get(uri: '/api/products?page=2')
+        ->assertStatus(status: 200)
+        ->assertExactJson([
+            'data' => array_splice($productList, 10, 20),
+            'links' => [
+                'first' => 'http://localhost/api/products?page=1',
+                'last' => 'http://localhost/api/products?page=2',
+                'prev' => 'http://localhost/api/products?page=1',
+                'next' => null,
+            ],
+            'meta' => [
+                'current_page' => 2,
+                'from' => 11 ,
+                'last_page' => 2,
+                'links' => [
+                    [
+                        'url' => 'http://localhost/api/products?page=1',
+                        'label' => '&laquo; Previous',
+                        'active' => false
+                    ],
+                    [
+                        'url' => 'http://localhost/api/products?page=1',
+                        'label' => '1',
+                        'active' => false
+                    ],
+                    [
+                        'url' => 'http://localhost/api/products?page=2',
+                        'label' => '2',
+                        'active' => true
+                    ],
+                    [
+                        'url' => null,
+                        'label' => 'Next &raquo;',
+                        'active' => false
+                    ]
+                ],
+                'path' => 'http://localhost/api/products',
+                'per_page' => 10,
+                'to' => 20,
+                'total' => 20
+            ],
+        ]);
+});
+
+it('should be return 302 if user is not auth', function () {
+    get(uri: '/api/products')
+        ->assertStatus(status: 302)
+        ->assertLocation('/api/login');
+});
